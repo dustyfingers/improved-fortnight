dd3fea74c5a02e8619ece867f86fa7a51cdde9cd
dustyfingers <dustyfingers01257@gmail.com>
2021-03-03 06:59:34 -0700

    end of video 1

--- music_controller/music_controller/urls.py
+++ music_controller/music_controller/urls.py
@@ -1,4 +1,4 @@
-"""music_controller_backend URL Configuration
+"""music_controller URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls'))
+    path('api/', include('api.urls'))
 ]
