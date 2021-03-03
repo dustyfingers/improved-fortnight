46262584a390fd7d19ea662a1049ba3be5d0da65
lou <dustyfingers01257@gmail.com>
2021-03-03 15:00:38 -0700

    end of video 5

--- music_controller/api/serializers.py
+++ music_controller/api/serializers.py
@@ -7,4 +7,7 @@ class RoomSerializer(serializers.ModelSerializer):
         fields = '__all__'
         # fields = ('id', 'code', 'host', 'guest_can_pause', 'votes_to_skip')
 
-        
+class CreateRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('guest_can_pause', 'votes_to_skip')
