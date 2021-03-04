89ce169cf47bbc6db3a72d5826dfe001f8a7229c
lou <dustyfingers01257@gmail.com>
2021-03-04 06:14:38 -0700

    end of video 6

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -14,7 +14,6 @@ export default class App extends Component {
     render() {
         return (
             <div>
-                <h1>TESTING REACT</h1>
                 <HomePage />
             </div>
         );
